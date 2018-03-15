d82ac3438fad9e4bfcd6c3db26d1c1e74de6b734
Bera <5622227+ahmedbera@users.noreply.github.com>
2018-03-15 14:50:43 +0300

    Initial

---
+++ lib/race_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class Race {
+  String id;
+  String title;
+  String date;
+  String time;
+  String country;
+  String city;
+  DateTime raceTime;
+  bool isCompleted = false;
+
+  Race(this.id, this.title, this.date, this.time, this.city, this.country) {
+    try {
+      raceTime = DateTime.parse(date + " " + time).toLocal();
+      if(raceTime.difference(new DateTime.now()) < new Duration(seconds: 1)) {
+        this.isCompleted = true;
+      }
+    } catch (e) {
+      raceTime = new DateTime(2018);
+      print(e);
+    }
+  }
+
+}
+
+class RaceCard extends StatefulWidget{
+  RaceCard({Key key, this.race}) : super(key: key);
+
+  final Race race;
+
+  @override
+  _RaceCardState createState() => new _RaceCardState();
+}
+
+class _RaceCardState extends State<RaceCard> {
+  Duration countDown = new Duration();
+  String _days = "";
+
+  void _calculateRemainingDays() {
+    if(!mounted)
+      return;
+      setState((){
+        countDown = widget.race.raceTime.toUtc().difference(new DateTime.now().toUtc()).abs();
+        _days = countDown.inDays.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _calculateRemainingDays();
+    return new Container(
+      decoration: new BoxDecoration(border: new Border(bottom: new BorderSide(color: Theme.of(context).dividerColor))),
+      child: new Padding(padding: new EdgeInsets.all(12.0),
+        child:new Row(
+          children: <Widget>[
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 8.0),
+              width: 90.0,
+              height: 48.0,
+              child: new Image.asset(
+                'assets/flags/' + widget.race.country + '.png',
+                height: 48.0,
+                width: 90.0,
+                alignment: Alignment.centerLeft,
+                fit: BoxFit.fill,
+              ),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(widget.race.title, textAlign: TextAlign.left,),
+                new Text(widget.race.raceTime.toLocal().toString()),
+                widget.race.isCompleted ?
+                  new Text("Race Completed") :
+                  new Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    new Text(_days, style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    new Text(" Days ", style: new TextStyle(fontSize: 18.0),)
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
