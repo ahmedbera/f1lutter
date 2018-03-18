144c0d79c201ad774a6e23ae5232753b8473c830
Bera <5622227+ahmedbera@users.noreply.github.com>
2018-03-18 15:43:50 +0300

    Dark mode, tap race to see remaining time

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'F1utter',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'F1utter'),
+      home: new MyHomePage(title: 'F1utter', swatch: Colors.blue),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title, this.swatch}) : super(key: key);
 
   final String title;
+  final ColorSwatch swatch;
 
   @override
   _MyHomePageState createState() {
@@ -41,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Brightness brightness = Brightness.light;
   bool isLoading = true;
   Race closestRace;
   Duration countDown = new Duration();
@@ -67,8 +58,15 @@ class _MyHomePageState extends State<MyHomePage> {
       res = JSON.decode(json);
       var races = res["MRData"]["RaceTable"]["Races"];
       for (var race in races) {
-        print(race["Circuit"]);
-        Race _race = new Race(race["Circuit"]["circuitId"], race["raceName"], race["date"], race["time"], race["Circuit"]["Location"]["locality"], race["Circuit"]["Location"]["country"]);
+
+        Race _race = new Race(
+          race["Circuit"]["circuitId"], 
+          race["raceName"], race["date"], 
+          race["time"], 
+          race["Circuit"]["Location"]["locality"], 
+          race["Circuit"]["Location"]["country"]
+        );
+        
         if(closestRace == null && !_race.isCompleted) {
           setState(() {
             closestRace = _race;
@@ -80,8 +78,6 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       res = 'Error getting IP address:\nHttp status ${response.statusCode}';
     }
-
-
   }
 
   _calculateRemaningTime() {
@@ -99,6 +95,28 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  _toggleTheme() {
+    if(this.brightness == Brightness.light) {
+      setState(() {
+        this.brightness = Brightness.dark;
+      });
+    } else {
+      setState(() {
+        this.brightness = Brightness.light;
+      });
+    }
+  }
+
+  void _handleRaceTap(Race race) {
+    setState(() {
+      this.closestRace = race;
+      countDown = closestRace.raceTime.toUtc().difference(new DateTime.now().toUtc()).abs();
+      remainingDays = countDown.inDays.toString();
+      remainingHours = (countDown.inHours % 24).toString();
+      remainingMinutes = (-countDown.inMinutes % 60).toString();
+      remainingSeconds = (countDown.inSeconds % 60).toString();
+    });
+  }
 
   @override
   initState() {
@@ -108,67 +126,82 @@ class _MyHomePageState extends State<MyHomePage> {
   
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
+    return new MaterialApp(
+      theme: new ThemeData( 
+        brightness: brightness,
+        primarySwatch: widget.swatch,
+        accentColor: Colors.tealAccent,
       ),
-      body: new Column(
-        children: <Widget>[
-          this.isLoading ? new Text("Loading") :
-          new Container(
-            color: Colors.grey.shade800,
-            child: new Padding(
-              padding: new EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child : new Row(
-                children: <Widget>[
-                  new Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      new Row(
-                        children: <Widget>[
-                          new Image.asset('assets/flags/'+ closestRace.country+'.png', height: 16.0,),
-                          new Text(" " + closestRace.city + " ", style: smallTextStyle),
-                          new Text(closestRace.country, style: smallTextStyle)
-                        ],
-                      ),
-                      new Container(
-                        margin: new EdgeInsets.symmetric(vertical: 4.0),
-                        child: new Column(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(widget.title),
+           actions: <Widget>[
+             new IconButton(
+               icon: new Icon(Icons.lightbulb_outline),
+               tooltip: "Toggle Theme",
+               onPressed: this._toggleTheme,
+             )
+           ],
+        ),
+        body: new Column(
+          children: <Widget>[
+            this.isLoading ? new Text("Loading") :
+            new Container(
+              color: Colors.grey.shade800,
+              child: new Padding(
+                padding: new EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child : new Row(
+                  children: <Widget>[
+                    new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Row(
                           children: <Widget>[
-                            new Text(closestRace.title, style: new TextStyle(fontSize: 22.0, color: Colors.white)),
-                            new Text(closestRace.raceTime.toLocal().toString(), style: new TextStyle(fontSize: 18.0, color: Colors.white70)),
+                            new Image.asset('assets/flags/'+ closestRace.country+'.png', height: 16.0,),
+                            new Text(" " + closestRace.city + ", ", style: smallTextStyle),
+                            new Text(closestRace.country, style: smallTextStyle)
                           ],
                         ),
-                      ),
-                      new Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          new Text(remainingDays, style: countdownIntStyle),
-                          new Text(" D ", style: countdownStrStyle,),
-                          new Text(remainingHours, style: countdownIntStyle),
-                          new Text(" H ", style: countdownStrStyle,),
-                          new Text(remainingMinutes, style: countdownIntStyle),
-                          new Text(" M ", style: countdownStrStyle,),
-                          new Text(remainingSeconds, style: countdownIntStyle),
-                          new Text(" S ", style: countdownStrStyle)
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                        new Container(
+                          margin: new EdgeInsets.symmetric(vertical: 4.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(closestRace.title, style: new TextStyle(fontSize: 22.0, color: Colors.white)),
+                              new Text(closestRace.raceTime.toLocal().toString(), style: new TextStyle(fontSize: 18.0, color: Colors.white70)),
+                            ],
+                          ),
+                        ),
+                        new Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            new Text(remainingDays, style: countdownIntStyle),
+                            new Text(" D ", style: countdownStrStyle,),
+                            new Text(remainingHours, style: countdownIntStyle),
+                            new Text(" H ", style: countdownStrStyle,),
+                            new Text(remainingMinutes, style: countdownIntStyle),
+                            new Text(" M ", style: countdownStrStyle,),
+                            new Text(remainingSeconds, style: countdownIntStyle),
+                            new Text(" S ", style: countdownStrStyle)
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          new Flexible (
-            child: new ListView.builder(
-              itemCount: raceList.length,
-              itemBuilder: (context,int index) {
-                Race currentRace = raceList[index];
-                return new RaceCard(race: currentRace);
-              },
+            new Flexible (
+              child: new ListView.builder(
+                itemCount: raceList.length,                 
+                itemBuilder: (context,int index) {
+                  Race currentRace = raceList[index];
+                  return new RaceCard(race: currentRace, onChanged: this._handleRaceTap);
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
