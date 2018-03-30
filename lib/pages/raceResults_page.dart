8778b2707dcb24344371d1ae1d60ddd851d8be42
Bera <5622227+ahmedbera@users.noreply.github.com>
2018-03-30 10:08:51 +0300

    Show race results

--- lib/pages/raceResults_page.dart
+++ lib/pages/raceResults_page.dart
@@ -12,11 +12,37 @@ class RaceResultPage extends StatefulWidget {
 }
 
 class RaceResultsPageState extends State<RaceResultPage> {
-  
+  List<RaceResult> raceResults = new List();
+  List<DataRow> dataRows = new List();
+
   @override
   void initState() {
     super.initState();
     ApiHelper.getRaceResultsByRound(year: widget.raceData.raceTime.year.toString(), round: widget.raceData.round).then((res) {
+
+      List<DataRow> rows = new List();
+      
+      for(var row in res) {
+        rows.add(
+          new DataRow(
+            cells: [
+              new DataCell(new Text(row.position)),
+              new DataCell(new Text(row.driver.givenName + " " + row.driver.familyName)),
+              new DataCell(new Text(row.grid)),
+              new DataCell(new Text(row.laps)),
+              new DataCell(new Text(row.status)),
+              new DataCell(new Text(row.time)),
+              new DataCell(new Text(row.fastestLapTime)),
+              new DataCell(new Text(row.avgSpeed))
+            ]
+          )
+        );
+      }
+      
+      this.setState(() {
+        raceResults = res;
+        this.dataRows = rows;
+      });
 
     });
   }
@@ -25,15 +51,26 @@ class RaceResultsPageState extends State<RaceResultPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Second Screen"),
+        title: new Text(widget.raceData.title),
       ),
-      body: new Center(
-        child: new RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: new Text(widget.raceData.round),
-        ),
+      body: new SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: new SingleChildScrollView(
+         scrollDirection: Axis.vertical,
+          child: new DataTable(
+            columns: [
+              new DataColumn(label: new Text("Pos")),
+              new DataColumn(label: new Text("Driver")),
+              new DataColumn(label: new Text("Grid")),
+              new DataColumn(label: new Text("Laps")),
+              new DataColumn(label: new Text("Status")),
+              new DataColumn(label: new Text("Time")),
+              new DataColumn(label: new Text("Fastest Lap")),
+              new DataColumn(label: new Text("Average Speed")),
+            ],
+            rows: this.dataRows,
+          ),
+        )
       ),
     );
   }
