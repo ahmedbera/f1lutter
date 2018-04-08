0a2e321deb2f121ef478382c0178a990c3f394fe
Bera <5622227+ahmedbera@users.noreply.github.com>
2018-04-08 21:10:19 +0300

    bug fix: now with more race results

--- lib/persistent_state.dart
+++ lib/persistent_state.dart
@@ -1,9 +1,15 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
-import 'package:f1utter/models/Race.dart';
+//import 'package:f1utter/models/Race.dart';
 
-class GlobalData {
-  static ValueNotifier<List<RaceResult>> raceResults = new ValueNotifier(new List<RaceResult>());
+class GlobalData extends ChangeNotifier{
+  static ValueNotifier<Map> raceResults = new ValueNotifier<Map<dynamic, dynamic>>(new Map());
   static ValueNotifier<Map> driverStandings = new ValueNotifier<Map<dynamic, dynamic>>(new Map());
   static ValueNotifier<Map> constructorStandings = new ValueNotifier<Map<dynamic, dynamic>>(new Map());
+
+  static void updateRaceResults(round, res) {
+    GlobalData.raceResults.value[round.toString()] = res;
+    GlobalData.raceResults.notifyListeners();
+  }
+
 }
