0eac138b62d263f8eafd96adcc432e4d80362e41
Bera <5622227+ahmedbera@users.noreply.github.com>
2018-05-05 11:29:14 +0300

    Customize color theme

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:f1utter/persistent_state.dart';
+import 'dart:async';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
@@ -13,7 +14,63 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   var selected;
-  
+
+  Future<Null> _colorDialog(colorType) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return new SimpleDialog(
+          contentPadding: new EdgeInsets.symmetric( horizontal: 8.0, vertical: 16.0),
+          title: new Text("Choose a color"),
+          children: <Widget>[
+            new Padding(
+              padding: new EdgeInsets.all(8.0),
+              child: new Wrap(
+                runAlignment: WrapAlignment.start,
+                runSpacing: 4.0,
+                spacing: 4.0,
+                children: colorType == "accent" ?
+                  Colors.accents.map((color){
+                    return new CircleAvatar(
+                      backgroundColor: color,
+                      child: new InkWell(
+                        onTap: () {
+                          GlobalData.accentColor.value = color;
+                          Navigator.pop(context);
+                        },
+                        child: GlobalData.accentColor.value == color ? new Icon(Icons.check) : null,
+                      )
+                    );
+                  }).toList() : 
+                  Colors.primaries.map((color){
+                    return new CircleAvatar(
+                      backgroundColor: color,
+                      child: new InkWell(
+                        onTap: () {
+                          GlobalData.primaryColor.value = color;
+                          Navigator.pop(context);
+                        },
+                        child: GlobalData.primaryColor.value == color ? new Icon(Icons.check) : null,
+                      )
+                    );
+                  }).toList(),
+              ),
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                new SimpleDialogOption(
+                  child: new Text("Cancel"),
+                  onPressed: () { Navigator.pop(context);},
+                )
+              ],  
+            )
+          ],
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Column(
@@ -26,9 +83,9 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
         new ListTile(
           title: new Text("Theme"),
-          leading: new Icon(Icons.lightbulb_outline),
+          subtitle: new Text("Choose your base theme (light or dark)"),
           trailing: new PopupMenuButton(
-            onSelected: (var value) { GlobalData.updateTheme(value); },
+            onSelected: (var value) { GlobalData.appBrightness.value = value; },
             itemBuilder: (BuildContext context) => <PopupMenuEntry<Brightness>>[
               const PopupMenuItem(
                 value: Brightness.dark,
@@ -39,6 +96,24 @@ class _SettingsPageState extends State<SettingsPage> {
                 child: const Text("Light"),
               ),
             ],
+          ),
+        ),
+        new Divider(),
+        new ListTile(
+          title: new Text("Primary Color"),
+          onTap: () {_colorDialog("primary");},
+          enabled: Theme.of(context).brightness == Brightness.light ? true : false,
+          subtitle: new Text("Only works with light theme"),
+          trailing: new CircleAvatar(
+             backgroundColor: Theme.of(context).primaryColor,
+          ),
+        ),
+        new Divider(),
+        new ListTile(
+          title:  new Text("Accent Color"),
+          onTap: () {_colorDialog("accent");},
+          trailing: new CircleAvatar(
+             backgroundColor: Theme.of(context).accentColor,
           ),
         ),
         new Divider(),
